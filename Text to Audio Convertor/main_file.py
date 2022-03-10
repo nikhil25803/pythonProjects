798c64a6f7a18693968284ec1e3baa3799a81b98
Nikhil Raj <nikhil.raj.aiml25@heritageit.edu.in>
2022-03-10 12:24:36 +0530

    Text-Audio Convertor

--- Text to Audio Convertor/main_file.py
+++ Text to Audio Convertor/main_file.py
@@ -1,14 +1,30 @@
 from gtts import gTTS
 
-# For a quick mp3 file
-# tts = gTTS('hello', lang='en')
-# tts.save('hello.mp3') 
+def play_sound(lang1, lang2, lang3, lang4, lang5):
+
+    lang1.save('english.mp3')
+
+    lang2.save('french.mp3')
+
+    lang3.save('mandarin.mp3')
+
+    lang4.save('portuguese.mp3')
+
+    lang5.save('spanish.mp3')
+
+    print("All mp3 files are created")
+
+
+english = gTTS('Good Morning', lang='en')
+french = gTTS('bon matin', lang='fr')
+mandarin = gTTS('Zǎoshang hǎo', lang='zh-TW')
+portuguese = gTTS('bom dia', 'pt')
+spanish = gTTS('buenos días', 'es')
+play_sound(english, french, mandarin, portuguese, spanish)
 
 from playsound import playsound
 
-audio='speech.mp3'
-language='en'
-sp=gTTS(text="Hello World ! My name is Nikhil Raj",lang=language,slow=False)
+audio=['english.mp3', 'french.mp3', 'mandarin.mp3', 'spanish.mp3']
 
-sp.save(audio)
-playsound(audio)
+for i in audio:
+    playsound(i)
