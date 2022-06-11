3b9b16462f9952a2fe3a7b08065dfe2df610c013
Nikhil Raj <nikhil25803@gmail.com>
2022-06-11 23:00:06 +0530

    Modified Sppedtester

--- Internet Speed Tester/main.py
+++ Internet Speed Tester/main.py
@@ -1,20 +1,25 @@
 import speedtest
 
-st=speedtest.Speedtest()
+test = speedtest.Speedtest()
+
 option = int(input('''What speed you want to test: 
 1. Download Speed
 2. Upload Speed
 3. Ping
-Your Choice : 
-'''))
+Your Choice :  '''))
 
 if option == 1:
-    print(st.download())
+    download_speed = test.download()
+    print(f"{download_speed} Mbps")
+
 elif option == 2:
-    print(st.upload())
+    upload_speed = test.upload()
+    print(f"{upload_speed} Mbps")
+
 elif option == 3:
-    servernames=[]
-    st.getservers(servernames)
-    print(st.results.ping)
+    servernames = []
+    test.get_servers(servernames)
+    print(test.results.ping)
+
 else:
     print("Enter a valid choice")
